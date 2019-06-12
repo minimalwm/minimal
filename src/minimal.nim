5fbbbff3e24e8a952ee197492bc944d2df490e66
jacobsin <jayrad.security@protonmail.com>
2019-06-12 17:01:42 -0600

    Initial setup of display object in minimal
    nothing too special here

--- src/minimal.nim
+++ src/minimal.nim
@@ -1,4 +1,19 @@
-import minimalpkg/submodule
+import
+  x11/x,
+  x11/xlib,
+  x11/xatom,
+  x11/xinerama
+
+var
+  display:PDisplay    # Display object for x11
 
 when isMainModule:
-  echo(getWelcomeMessage())
+  display = XOpenDisplay(nil)
+  # nil here will ensure that it opens
+  # the default display
+
+  if display.isNil:
+    quit("Failed to open Display", QuitFailure)
+
+  discard XCloseDisplay(display)
+  # Should find way to always ensure this procedure is called.
