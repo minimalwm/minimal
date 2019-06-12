a31c16bb67168691600572ccfd5e1da6c5b356c6
jacobsin <jayrad.security@protonmail.com>
2019-06-12 16:42:35 -0600

    Basic comment removal

--- src/minimal.nim
+++ src/minimal.nim
@@ -1,6 +1,3 @@
-# This is just an example to get you started. A typical hybrid package
-# uses this file as the main entry point of the application.
-
 import minimalpkg/submodule
 
 when isMainModule:
