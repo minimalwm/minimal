d48fd3bdfa0bf72fa99d06eec4aa2d7efd9d2793
jacobsin <jayrad.security@protonmail.com>
2019-06-12 18:01:32 -0600

    Slowly progressing through a setup
    Must get some basic information before starting the window manager. Getting there

--- src/minimal.nim
+++ src/minimal.nim
@@ -8,6 +8,11 @@ import
 
 var
   display:PDisplay    # Display object for x11
+  root:TWindow        # Default root window for display
+  attr:TXWindowAttributes
+  screen:cint
+  screen_width:cint
+  screen_height:cint
 
 when isMainModule:
   display = XOpenDisplay(nil)
@@ -19,8 +24,18 @@ when isMainModule:
 
   var xinerama_status = XineramaIsActive(display)
 
-  if xinerama_status.isFalse:
+  if xinerama_status.isFalse:   
+    # Is there a better way to do this and I just suck.
     quit("Xinerama is not loaded.", QuitFailure)
+
+  screen = DefaultScreen(display)
+  screen_width = DisplayWidth(display, screen)
+  screen_height = DisplayHeight(display, screen)
+  
+  root = RootWindow(display, screen)
+  
+  # Get monitors
+  # 
 
   discard XCloseDisplay(display)
   # Should find way to always ensure this procedure is called.
