76b5372fd2ba1f26ca7f3ae8d792b14900162317
Anirudh Oppiliappan <x@icyphox.sh>
2019-09-01 01:53:55 +0530

    Process keycode as strings
    Signed-off-by: Anirudh Oppiliappan <x@icyphox.sh>

--- src/minimal.nim
+++ src/minimal.nim
@@ -7,23 +7,35 @@ var
   start: TXButtonEvent
   ev: TXEvent
   attr: TXWindowAttributes
+  ksym: TKeySym
+  kstr: cstring
 
-if dpy == nil: quit 1
 
-discard dpy.XGrabKey(dpy.XKeysymToKeycode(XK_Return).cint, Mod4Mask, dpy.XDefaultRootWindow, 1, GrabModeAsync, GrabModeAsync)
+if dpy == nil:
+  echo "error: failed to open X display"
+  quit 1
+
+# mouse actions
 discard dpy.XGrabButton(1, Mod4Mask, dpy.XDefaultRootWindow, 1, ButtonPressMask or ButtonReleaseMask or PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None)
 discard dpy.XGrabButton(3, Mod4Mask, dpy.XDefaultRootWindow, 1, ButtonPressMask or ButtonReleaseMask or PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None)
+
+# FIXME: throws BadWindow, also closes WM session
 discard dpy.XGrabKey(dpy.XKeysymToKeycode(XStringToKeysym("q")).cint, Mod4Mask, dpy.XDefaultRootWindow, 1, GrabModeAsync, GrabModeAsync)
+discard dpy.XGrabKey(dpy.XKeysymToKeycode(XK_Return).cint, Mod4Mask, dpy.XDefaultRootWindow, 1, GrabModeAsync, GrabModeAsync)
 
 while true:
+
   discard dpy.XNextEvent(addr ev)
-  # echo "Type: ", ev.theType, ", subwindow: ", ev.xkey.subwindow, " ", ev.xbutton.subwindow, " ", start.subwindow
-  # echo "keycode: ", ev.xkey.keycode
+  echo "Type: ", ev.theType, ", subwindow: ", ev.xkey.subwindow, " ", ev.xbutton.subwindow, " ", start.subwindow
+  echo "keycode: ", ev.xkey.keycode
   if ev.theType == KeyPress and ev.xkey.window.culong != None:
-    case ev.xkey.keycode:
-      of QUIT:
+    ksym = XKeycodeToKeysym(dpy, cast[TKeyCode](ev.xkey.keycode), 0)
+    kstr = XKeysymToString(ksym)
+    echo "keystring: ", kstr
+    case $kstr:
+      of "q":
         discard dpy.XDestroyWindow(ev.xkey.subwindow)
-      of TERM:
+      of "Return":
         var l = @["xterm"].allocCStringArray
         spawn(l)
         l.deallocCStringArray
@@ -31,6 +43,7 @@ while true:
         continue
   elif ev.theType == ButtonPress and ev.xbutton.subwindow.culong != None:
     discard dpy.XGetWindowAttributes(ev.xbutton.subwindow, addr attr)
+    discard dpy.XRaiseWindow(ev.xkey.subwindow)
     start = ev.xbutton
   elif ev.theType == MotionNotify and start.subwindow.culong != None:
     let
